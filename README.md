b156f57735d76e7fec1c331eca17b0ff43cd39e6
Mohamed Miloudi <milou0104@gmail.com>
2019-07-13 18:24:17 +0100

    WIP

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 # Classic Arcade Game Clone Project
 
-## Table of Contents
+This is a simple arcade game built using JavaScipt
 
-- [Instructions](#instructions)
-- [Contributing](#contributing)
+![Game Screenshot](screenshots/img.jpg "Game Screenshot")
 
-## Instructions
+## How to play
+First you have to chose your character before starting the game.
 
-Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
+Then use the arrow keys **UP**, **DOWN**, **LEFT** and **RIGHT** to move the character
 
-Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
+You have three lives during your game, each time an enemy hits you, you'll lose one live and once you got 0 lives your game will over
 
-Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
+Your mission is to collect the **3 stars** and reach the sea the increments your score
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+Each time your score increments there's a change to get a supply 
 
-## Contributing
+There are four type of supplies: 
+* `The Blue Gem:` makes you invisible for 3 seconds from the time an enemy hits you.
+* `The Green Gem:` Freeze all the enemies for 3 seconds once you collect it. 
+* `The Orange Gem:` Will increments your stars up to three, so once you collect it you just need to get to the sea.
+* `The Heart:`  Will increments you lives.
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+**Note:**
+>Your high score at the end of the game will always be stored.
+
+## Credirs
+
+_Font_: [Font Awesome](https://fontawesome.com/)
+
+_Sound Effects_: [Freesound](https://freesound.org/)
--- js/app.js
+++ js/app.js
@@ -4,40 +4,44 @@
 
 // The image/sprite for our enemies, this uses
 // a helper we've provided to easily load images
+let canPlay = false;
+
 class Enemy {
-    constructor () {
-        this.sprite = 'images/enemy-bug.png';
-        this.resetPosition()
+  constructor () {
+    this.sprite = 'images/enemy-bug.png';
+    this.resetPosition ();
+  }
+
+  // Draw the enemy on the screen, required method for game
+  render () {
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * (171 / 2) + -25);
+  }
+
+  update (dt) {
+
+    if (game.freezeEnemies) {
+      return;
     }
 
-    // Draw the enemy on the screen, required method for game
-    render () {
-        ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * (171/2) + -25);
+    if (this.x >= 5) {
+      this.resetPosition();
     }
 
-    update (dt) {
+    this.x += (dt * this.speed);
 
-        if (game.freezeEnemies) {
-            return;
-        }
-        if (this.x >= 5 ) {
-            this.resetPosition()
-        }
-        this.x += (dt * this.speed)
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    //
+  }
 
-        // Update the enemy's position, required method for game
-        // Parameter: dt, a time delta between ticks
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
-        // all computers.
-        //
-    }
-
-    resetPosition () {
-        this.x = -1
-        this.y = Math.floor(Math.random() * 3 + 1)
-        this.speed = Math.ceil(Math.random() * 4)
-    }
+  resetPosition () {
+    this.x = -1;
+    this.y = Math.floor(Math.random() * 3 + 1);
+    this.speed = Math.ceil(Math.random() * 4);
+  }
 
 }
 
@@ -50,66 +54,69 @@ class Enemy {
  */
 class Player {
 
-    constructor () {
-        this.sprite = 'images/char-cat-girl.png';
-        this.x = 2
-        this.y = 5
+  constructor () {
+    this.sprite = 'images/char-cat-girl.png';
+    this.x = 2;
+    this.y = 5;
+  }
+
+  render () {
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * (171 / 2) + -25);
+  }
+
+  update (dt) {
+    if (this.y === 0 && game.hasAllStars()) {
+      this.gain();
+    }
+  }
+
+  handleInput (e) {
+
+    if (canPlay === false) {
+      return;
     }
 
-    render () {
-
-        ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * (171/2) + -25)
+    if (e === 'left' && this.x > 0) {
+      this.x--;
     }
 
-    update (dt) {
-        if (this.y === 0 && game.hasAllStars()) {
-            this.gain()
-        }
+    if (e === 'right' && this.x < 4) {
+      this.x++;
     }
 
-    handleInput(e) {
-        if (e === 'left' && this.x > 0) {
-            this.x--
-        }
+    if (e === 'up' && this.y > 0) {
+      if (this.y === 1 && game.superStar) {
+        this.gain();
+        return;
+      }
 
-        if (e === 'right' && this.x < 4) {
-            this.x++
-        }
-
-        if (e === 'up' && this.y > 0) {
-            if (this.y === 1 && game.superStar) {
-                this.gain()
-                return;
-            }
-
-            if (this.y === 1 && !game.hasAllStars()) {
-                return
-            }
-            this.y--
-        }
-
-        if (e === 'down' && this.y < 5) {
-            this.y++
-        }
+      if (this.y === 1 && !game.hasAllStars()) {
+        return;
+      }
+      this.y--;
     }
 
-    resetPlayer () {
-        this.x = 2
-        this.y = 5
+    if (e === 'down' && this.y < 5) {
+      this.y++;
     }
+  }
 
-    gain () {
-        game.incrementScore();
-        this.resetPlayer()
-        console.log('player gain')
-    }
+  resetPlayer () {
+    this.x = 2;
+    this.y = 5;
+  }
 
-    catched () {
-        if (!game.hasProtection) {
-            this.resetPlayer()
-        }
-        game.playerCatched()
+  gain () {
+    game.incrementScore();
+    this.resetPlayer();
+  }
+
+  catched () {
+    if (!game.hasProtection) {
+      this.resetPlayer();
     }
+    game.playerCatched();
+  }
 }
 
 /**
@@ -117,210 +124,235 @@ class Player {
  */
 class Game {
 
-    lives = 3;
-    stars = 0
-    score = 0;
-    supplyType = '';
-    hasSupplies = false;
-    hasProtection = false;
-    freezeEnemies = false;
-    superStar = false;
+  lives = 3;
+  stars = 0;
+  score = 0;
+  supplyType = '';
+  hasSupplies = false;
+  hasProtection = false;
+  freezeEnemies = false;
+  superStar = false;
 
-    constructor () {
-        this.sprite = 'images/star.png';
-        this.audio = new Audio;
-        this.resetPosition()
-        this.checkForSupplies()
+  constructor () {
+    this.sprite = 'images/star.png';
+    this.audio = new Audio;
+    this.resetPosition();
+    this.checkForSupplies();
+  }
+
+  render () {
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * (171 / 2) + -25);
+
+    if (this.hasSupplies) {
+      this.gameSupplies();
     }
 
-    render () {
-        ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * (171/2) + -25);
+  }
 
-        if (this.hasSupplies) {
-            this.gameSupplies()
-        }
+  update(dt) {}
 
+  starCollected () {
+
+    if (this.hasSupplies) {
+      this.gameSupplies()
     }
 
-    update (dt) {
-        //
-        //
+    this.runAudio('effects/start-collected.wav');
+
+    this.stars++;
+
+      renderStars();
+
+    if (this.stars < 3) {
+      this.resetPosition();
+      return;
     }
 
-    starCollected () {
+    this.y = -999;
+  }
 
-        this.gameSupplies()
+  hasAllStars () {
+    return this.stars === 3;
+  }
 
-        this.runAudio('effects/start-collected.wav');
+  resetPosition () {
+    this.x = Math.floor(Math.random() * 5);
+    this.y = Math.floor(Math.random() * 3 + 1);
+  }
 
-        this.stars++
+  resetStars () {
+    this.stars = 0;
+    this.superStar = false;
+    this.resetPosition();
+  }
 
-        renderStars()
+  incrementScore () {
+    this.score++;
+    this.resetStars();
+    renderStars();
+    renderScore();
+    this.checkForSupplies();
+    this.runAudio('effects/sea-reached.wav');
+  }
 
-        if (this.stars < 3) {
-            this.resetPosition()
-            return;
-        }
+  playerCatched (src) {
+    if (this.hasProtection) {
+      setTimeout(() => {
+        this.hasProtection = false;
+        renderProtection();
+      }, 3000);
 
-        this.y = -999
+      return;
     }
 
-    hasAllStars () {
-        return this.stars === 3;
+    this.lives--;
+    this.resetStars();
+    renderHeart();
+    renderStars();
+
+    if (this.lives === 0) {
+      this.gameover();
+      return;
     }
 
-    resetPosition () {
-        this.x = Math.floor(Math.random() * 5)
-        this.y = Math.floor(Math.random() * 3 + 1)
+    this.runAudio('effects/catched.mp3');
+  }
+
+  gameover () {
+    this.resetStars();
+    this.saveToLocalstorage();
+    canPlay = false;
+
+    document.querySelector('.score').innerText = this.score;
+    document.querySelector('.highscore').innerText = this.highscore();
+    document.querySelector('.gameover').style.display = 'flex';
+
+    this.score = 0;
+    this.lives = 3;
+    renderHeart();
+    renderStars();
+    this.runAudio('effects/game-over.wav');
+  }
+
+  saveToLocalstorage () {
+    localStorage.setItem(
+      'score',
+      Math.max(this.score, localStorage.getItem('score'))
+    );
+  }
+
+  highscore () {
+    return Math.max(this.score, localStorage.getItem('score'));
+  }
+
+  // Each time a player increments its score
+  // there is a small change a supply may appear
+  // Chances are 5/100
+  checkForSupplies () {
+    let random = Math.floor(Math.random() * 100 + 1);
+
+    if (random > 0 && random <= 5) {
+      this.supplyType = 'images/gem-blue.png';
+      this.supplyPosition();
+      this.hasSupplies = true;
+      return;
     }
 
-    resetStars () {
-        this.stars = 0
-        this.resetPosition()
+    if (random > 35 && random <= 40) {
+      this.supplyType = 'images/gem-green.png';
+      this.supplyPosition();
+      this.hasSupplies = true;
+      return;
     }
 
-    incrementScore () {
-        this.score ++;
-        this.superStar = false
-        renderStars()
-        renderScore()
-        this.resetStars()
-        this.runAudio('effects/sea-reached.wav');
-        this.checkForSupplies()
+    if (random > 70 && random <= 75) {
+      this.supplyType = 'images/gem-orange.png';
+      this.supplyPosition();
+      this.hasSupplies = true;
+      return;
     }
 
-    playerCatched (src) {
-        if (this.hasProtection) {
-            setTimeout(() => {
-                this.hasProtection = false
-                renderProtection()
-            }, 3000)
-
-            return;
-        }
-
-        this.lives--
-        this.resetStars()
-        renderHeart()
-        renderStars()
-        console.log(`Lives: ${this.lives}`)
-
-        if (this.lives === 0) {
-            this.gameover()
-            return;
-        }
-
-        this.runAudio('effects/catched.mp3');
+    if (random > 95 && random <= 100) {
+      this.supplyType = 'images/heart.png';
+      this.supplyPosition();
+      this.hasSupplies = true;
+      return;
     }
 
-    gameover (src) {
-        this.resetStars ()
-        this.runAudio('effects/game-over.wav');
-        console.log('Game Over!')
+    this.supplyType = ''
+  }
+
+  supplyCollected () {
+    this.supplyY = -999;
+    switch (this.supplyType) {
+      case 'images/gem-blue.png':
+        this.hasProtection = true;
+        break;
+
+      case 'images/gem-green.png':
+        this.freezeEnemies = true;
+
+        setTimeout(() => {
+          this.freezeEnemies = false;
+        }, 3000);
+        break;
+
+      case 'images/gem-orange.png':
+        this.superStar = true;
+        this.y = -999;
+        this.stars = 3;
+        renderStars();
+        break;
+
+      case 'images/heart.png':
+        this.lives++;
+        renderHeart();
+        break;
     }
 
-    checkForSupplies () {
-        // let random = Math.floor(Math.random() * 100 + 1);
-        let random = 74;
+    renderProtection();
+    this.runAudio('effects/supply-collected.wav');
+  }
 
-        console.log(random)
+  gameSupplies () {
+    ctx.drawImage(Resources.get(this.supplyType), this.supplyX * 101, this.supplyY * (171 / 2) + -25);
+  }
 
-        if (random > 0 && random <= 5) {
-            this.supplyType = 'images/gem-blue.png'
-            this.supplyPosition()
-            this.hasSupplies = true
-        }
+  supplyPosition () {
+    this.supplyX = Math.floor(Math.random() * 5);
+    this.supplyY = Math.floor(Math.random() * 3 + 1);
+  }
 
-        if (random > 35 && random <= 40) {
-            this.supplyType = 'images/gem-green.png'
-            this.supplyPosition()
-            this.hasSupplies = true
-        }
-
-        if (random > 70 && random <= 75) {
-            this.supplyType = 'images/gem-orange.png'
-            this.supplyPosition()
-            this.hasSupplies = true
-        }
-
-        if (random > 95 && random <= 100) {
-            this.supplyType = 'images/heart.png'
-            this.supplyPosition()
-            this.hasSupplies = true
-        }
-    }
-
-    supplyCollected () {
-        switch (this.supplyType) {
-            case 'images/gem-blue.png':
-                this.hasProtection = true
-                break;
-
-            case 'images/gem-green.png':
-                this.freezeEnemies = true
-                setTimeout(() => {
-                    this.freezeEnemies = false
-                }, 3000)
-                break;
-
-            case 'images/gem-orange.png':
-                this.superStar = true
-                this.y = -999
-                this.supplyY = -999
-                this.stars = 3
-                renderStars()
-                break;
-
-            case 'images/heart.png':
-                this.lives++
-                renderHeart()
-                break;
-        }
-        renderProtection()
-        this.runAudio('effects/supply-collected.wav');
-    }
-
-    gameSupplies () {
-        ctx.drawImage(Resources.get(this.supplyType), this.supplyX * 101, this.supplyY * (171/2) + -25);
-    }
-
-    supplyPosition () {
-        this.supplyX = Math.floor(Math.random() * 5)
-        this.supplyY = Math.floor(Math.random() * 3 + 1)
-    }
-
-    runAudio (src) {
-        this.audio.src = src;
-        this.audio.play();
-    }
+  runAudio (src) {
+    this.audio.src = src;
+    this.audio.play();
+  }
 }
-
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 const allEnemies = [
-        new Enemy(),
-        // new Enemy(),
-        // new Enemy(),
-    ];
+  new Enemy(),
+  new Enemy(),
+  new Enemy(),
+];
 
 const player = new Player();
 const game = new Game();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener('keyup', function (e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
 
 const livesEl = document.querySelector('.table__lives');
@@ -329,28 +361,50 @@ const starsEl = document.querySelector('.table__stars');
 const protectionEl = document.querySelector('.table__protection');
 
 const renderProtection = () => {
-    protectionEl.innerText = game.hasProtection === true ? 'True' : 'False'
+  protectionEl.innerText = game.hasProtection === true ? 'True' : 'False';
 }
 
 const renderStars = () => {
-    starsEl.innerText = game.stars
+  starsEl.innerText = game.stars;
 }
 
 const renderScore = () => {
-    scoreEl.innerText = game.score
+  scoreEl.innerText = game.score;
 }
 
 const renderHeart = function () {
 
-    livesEl.innerText = ''
-    const fragment = document.createDocumentFragment()
-    for(let i = 0; i < game.lives; i++){
-        const icon = document.createElement('icon')
-        icon.classList = ['fa fa-heart fa-lg bg-danger mx-1'];
-        fragment.appendChild(icon);
-    }
+  livesEl.innerText = '';
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < game.lives; i++) {
+    const icon = document.createElement('icon');
+    icon.classList = ['fa fa-heart fa-lg bg-danger mx-1'];
+    fragment.appendChild(icon);
+  }
 
-    livesEl.appendChild(fragment)
+  livesEl.appendChild(fragment);
 }
 
-renderHeart()
+renderHeart();
+
+/* Adding player character event listener */
+const chars = document.querySelectorAll('.player-character');
+
+chars.forEach(char => {
+  char.addEventListener('click', playerCharacter);
+});
+
+function playerCharacter(event) {
+  event.preventDefault();
+  player.sprite = event.target.dataset.name;
+}
+
+const launchGame = () => {
+  canPlay = true;
+  document.querySelector('.wrapper').style.display = 'none';
+}
+
+const restartGame = () => {
+  document.querySelector('.gameover').style.display = 'none';
+  document.querySelector('.wrapper').style.display = 'flex';
+}
